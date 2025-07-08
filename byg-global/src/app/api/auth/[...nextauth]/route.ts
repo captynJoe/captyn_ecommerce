a520f1432d96877c1782d4e479810e187c506c9f
Captyn Joe <joelnjeng@gmail.com>
2025-07-08 16:15:51 -0700

    mongodebate

--- byg-global/src/app/api/auth/[...nextauth]/route.ts
+++ byg-global/src/app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,10 @@
+// This file is deprecated and has been disabled by renaming to route.ts.bak
 // NextAuth has been removed in favor of Firebase Auth
 // This file is deprecated and should not be used
-export async function GET() {
-  return new Response('NextAuth has been replaced with Firebase Auth', { status: 404 });
-}
+// export async function GET() {
+//   return new Response('NextAuth has been replaced with Firebase Auth', { status: 404 });
+// }
 
-export async function POST() {
-  return new Response('NextAuth has been replaced with Firebase Auth', { status: 404 });
-}
+// export async function POST() {
+//   return new Response('NextAuth has been replaced with Firebase Auth', { status: 404 });
+// }
